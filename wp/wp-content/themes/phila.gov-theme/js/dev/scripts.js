241eec2f3e49ab02052295ca0639cf02b0dee52b
Karissa Demi <k@karissademi.com>
2016-01-06 15:54:44 -0500

    Remove spaces

--- wp/wp-content/themes/phila.gov-theme/js/dev/scripts.js
+++ wp/wp-content/themes/phila.gov-theme/js/dev/scripts.js
@@ -6,12 +6,12 @@
 
 //department filter list
 new List('filter-list', {
-    valueNames: ['item', 'item-desc']
+  valueNames: ['item', 'item-desc']
 });
 
 jQuery(document).ready(function($) {
   $(".clickable-row").click(function() {
-      window.document.location = $(this).data("href");
+    window.document.location = $(this).data("href");
   });
   var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
   if ( w <= 640 ){
