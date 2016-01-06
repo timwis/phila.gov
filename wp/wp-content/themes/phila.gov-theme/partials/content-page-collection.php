acc690c33e5e3f05d5778f6199869101cb9bc845
Karissa Demi <k@karissademi.com>
2016-01-06 15:55:50 -0500

    Simplify collection page markup

--- wp/wp-content/themes/phila.gov-theme/partials/content-page-collection.php
+++ wp/wp-content/themes/phila.gov-theme/partials/content-page-collection.php
@@ -4,46 +4,14 @@
  *
  * @package phila-gov
  */
-	$walker_menu = new Content_Collection_Walker();
-  $connected_sections = new WP_Query( array(
-    'connected_type' => 'pages_to_sections',
-    'connected_items' => get_queried_object(),
-    'nopaging' => true,
-  ) );
-
-
-
-	if($post->post_parent) {
-			$children = wp_list_pages(array(
-				'include' => $connected_sections->ID,
-				'walker' => $walker_menu
-				));
-			$page_title = get_the_title($post->post_parent);
-		} ?>
-		<div class="data-swiftype-index='true'">
-		<div class="row">
-			<header class="entry-header small-24 columns">
-				<h1><?php echo $page_title; ?></h1>
-			</header>
-		</div>
-		<article id="post-<?php the_ID(); ?>">
-			<div class="row">
-				<div class="small-24 columns">
-						<aside>
-							<ul class="tabs vertical">
-								<?php	echo $children; ?>
-							</ul>
-						</aside>
-		    <div data-swiftype-name="body" data-swiftype-type="text" class="entry-content tabs-content">
-		      <div class="content active">
-						<header class="entry-header">
-							<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
-						</header><!-- .entry-header -->
-					  <?php the_content(); ?>
-					</div>
-		    </div><!-- .entry-content -->
-			</div>
-		</div>
-	</div>
-		<?php get_template_part( 'partials/content', 'modified' ) ?>
-	</article><!-- #post-## -->
+ ?>
+<?php $post_slug = $post->post_name; ?>
+<section id="<?php echo $post_slug  ?>" class="content">
+  <header class="entry-header">
+    <?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+  </header><!-- .entry-header -->
+  <div data-swiftype-name="body" data-swiftype-type="text" class="entry-content">
+    <?php the_content(); ?>
+  </div>
+</section><!-- #post-## -->
+<hr>
