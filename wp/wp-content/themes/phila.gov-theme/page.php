154e0ef3327668ffdac982aada151f5d764d5094
Karissa Demi <k@karissademi.com>
2016-01-06 15:55:10 -0500

    Update page template to support connections

--- wp/wp-content/themes/phila.gov-theme/page.php
+++ wp/wp-content/themes/phila.gov-theme/page.php
@@ -1,52 +1,78 @@
 <?php
 /**
- * The template for displaying all pages.
+ * The template for displaying all Pages.
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
+ * This will display either a content collection or a
  *
  * @package phila-gov
  */
 
 get_header();
- // Find connected pages
- $connected_sections = new WP_Query( array(
-   'connected_type' => 'pages_to_sections',
-   'connected_items' => get_queried_object(),
-   'nopaging' => true,
- ) );
+$connected_sections = new WP_Query( array(
+  'connected_type' => 'pages_to_sections',
+  'connected_items' => get_queried_object(),
+  'nopaging' => true,
+  'connected_direction' => 'from'
+  )
+);
 
 ?>
-  <div id="primary" class="content-area row">
-    <main id="main" class="site-main small-24 columns" role="main">
+<div id="primary" class="content-area row">
+  <main id="main" class="site-main small-24 columns" role="main">
 
-      <?php  // Display connected pages
-       if ( $connected_sections->have_posts() ) : ?>
-       <ul>
-       <?php while ( $connected_sections->have_posts() ) : $connected_sections->the_post();
+    <?php if ( have_posts() ) : ?>
 
-        $this_content = get_the_content();
+     <?php while ( have_posts() ) : the_post(); ?>
 
-        if ( count( $connected_sections ) != 0 && ( $this_content == 0 ) )  {
-            //this page is a parent, with content
+        <div class="row">
+          <header class="entry-header small-24 columns">
+            <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+          </header><!-- .entry-header -->
+        </div>
 
-            get_template_part( 'partials/content', 'page-collection' );
-        }elseif(($post->id = $post->post_parent)) {
-            get_template_part( 'partials/content', 'page-collection' );
-        }else {
-            get_template_part( 'partials/content', 'page' );
-          }
+       <?php endwhile; ?>
+     <?php endif; ?>
 
-        endwhile;
+     <div class="row">
+       <nav class="medium-8 columns">
 
-      endif;
-      wp_reset_postdata();
+        <?php // Display connected pages
+        if ( $connected_sections->have_posts() ) : ?>
 
-      ?>
+          <ul class="tabs vertical">
+            <li class="tab-title"><a href="<?php get_permalink(); ?>"> Overview </a></li>
+            <?php while ( $connected_sections->have_posts() ) : $connected_sections->the_post();
 
-    </main><!-- #main -->
-  </div><!-- #primary -->
+                get_template_part('partials/content', 'nav' );
 
+             endwhile; ?>
+          </ul>
+         </nav>
+         <div class="medium-16 columns">
+
+          <?php //body content
+            if ( have_posts() ) : ?>
+            <?php while ( have_posts() ) : the_post();
+
+              echo '<h2>Overview</h2>';
+              the_content(); ?>
+              <hr>
+
+              <?php endwhile; ?>
+            <?php endif;
+
+            while ( $connected_sections->have_posts() ) : $connected_sections->the_post();
+
+              get_template_part( 'partials/content', 'page-collection' );
+
+            endwhile; ?>
+
+      <?php endif; ?>
+      </div>
+    </div>
+    <?php wp_reset_postdata(); ?>
+
+  </main><!-- #main -->
+</div><!-- #primary -->
+<?php get_template_part( 'partials/content', 'modified' ) ?>
 <?php get_footer(); ?>
