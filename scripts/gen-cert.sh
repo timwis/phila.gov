a52ebaebf66591ed14882dac27cd030ab17273d0
timwis <tim@timwis.com>
2018-07-25 07:43:47 -0400

    Persist generated SSL certificate in docker-compose

--- scripts/gen-cert.sh
+++ scripts/gen-cert.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# If certs already exist, return
+if [ -f /etc/ssl/private/self-signed.key ]; then
+  echo "SSL certificate already exists. Skipping."
+  return
+fi
+
 echo "Generating SSL certificate"
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
   -keyout /etc/ssl/private/self-signed.key \
